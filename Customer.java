849acbed5f33e3be0cadc08f0c443c2217eed433
Sumer kumar <91982125+sumer82@users.noreply.github.com>
2023-02-02 16:43:45 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,16 @@
+package shopping;
+
+public class Customer {
+	
+	public String name;
+	public String email;
+	public String Phone;
+	public Customer(String name, String email, String phone) {
+		this.name = name;
+		this.email = email;
+		this.Phone = phone;
+	}
+	
+	
+
+}
